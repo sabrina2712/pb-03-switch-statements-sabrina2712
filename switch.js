ed6c61358d0b8534b737b2076a0dcae8c1596149
sabrina2712 <mailroshni@gmail.com>
2019-09-18 07:34:33 -0400

    add solutions of switch

---
+++ switch.js
@@ -0,0 +1,72 @@
+// Prob.1 Color Analyzer
+var analyzeColor = "Red";
+
+switch (analyzeColor) {
+  case "Red":
+    console.log("Red is a primary color!");
+    break;
+
+  case "Blue":
+    console.log("Blue is sky color!");
+    break;
+
+  case "Green":
+    console.log("Green is tree color!");
+    break;
+
+  case "Yellow":
+    console.log("Yellow is yolk color!");
+    break;
+
+  default:
+    console.log("i love all color");
+}
+
+// Prob. 2
+var grade = "B";
+
+switch (grade) {
+  case "A+":
+    console.log("Excellent");
+    break;
+
+  case "A":
+    console.log("Great Job!");
+    break;
+
+  case "B+":
+    console.log("Good!");
+    break;
+
+  case "B":
+    console.log("Average!");
+    break;
+
+  default:
+    console.log("Keep Trying!");
+}
+
+// Prob. 3 fruits
+
+var fruit = "Banana";
+
+switch (fruit) {
+  case "Apple":
+    console.log("Super Healthy!");
+    break;
+
+  case "Orange":
+    console.log("Great for Winter!");
+    break;
+
+  case "Strawberry":
+    console.log("Taste good!");
+    break;
+
+  case "Banana":
+    console.log("I love it!");
+    break;
+
+  default:
+    console.log("All of them are fruits!");
+}
